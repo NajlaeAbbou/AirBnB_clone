8f15805e6f4f2b65f91f91ddd939ec4bda95f632
Imane47250 <elaceriimane@gmail.com>
2023-09-16 10:16:20 -0700

    city class

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" City Module for AirBnb project """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ city class """
+    state_id = ""
+    name = ""

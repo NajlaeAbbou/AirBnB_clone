3d70acebebca89d83655f721c759a9d3dc474ca3
Imane47250 <elaceriimane@gmail.com>
2023-09-16 12:13:54 -0700

    pycode and exec

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """Serializes inst to a JSON file and deserializes
     JSON file to inst

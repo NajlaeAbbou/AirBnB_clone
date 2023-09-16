e558e40aee32262723f06a94a368a392bd34d3e9
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-09-16 14:28:22 -0700

    test engine

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""unittests for console.py"""
+import os
+import sys
+import unittest
+from console import HBNBCommand
+from io import StringIO
+from models import storage
+from models.engine.file_storage import FileStorage
+from unittest.mock import patch
+
+
+if __name__ == "__main__":
+    unittest.main()
